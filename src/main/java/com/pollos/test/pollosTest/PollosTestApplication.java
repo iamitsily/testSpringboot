61d3e057b620d1d4944cff80f8270f1adce626c2
iamitsily <ilystx1@gmail.com>
2023-12-04 00:54:10 -0600

    first commit

---
+++ src/main/java/com/pollos/test/pollosTest/PollosTestApplication.java
@@ -0,0 +1,13 @@
+package com.pollos.test.pollosTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PollosTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PollosTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pollos/test/pollosTest/PollosTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pollos.test.pollosTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PollosTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
